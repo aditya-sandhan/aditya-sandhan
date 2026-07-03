a128a9e95f852c89bc5d9755cf3e7057b8f95f1e
Tanujamj <tanujajadhav2612@gmail.com>
2026-07-03 09:32:35 +0530

    Add ArrayCC class for marks input and update functionality

---
+++ ArrayCC.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+
+public class ArrayCC {
+
+    public static void main(String args[]){
+        int marks[] = new int[10];
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter marks of Physics: ");      
+        marks[0]= sc.nextInt();
+        System.out.println("Enter marks of Chemistry: ");
+        marks[1] = sc.nextInt();
+        System.out.println("Enter marks of Maths: ");
+        marks[2] = sc.nextInt();
+        System.out.println("Marks of Physics: " + marks[0]);
+        System.out.println("Marks of Chemistry: " + marks[1]);
+        System.out.println("Marks of Maths: " + marks[2]);  
+
+
+        marks[2] = marks[2] + 5;
+        System.out.println("updated Marks of Maths: " + marks[2]);  
+    }
+}
